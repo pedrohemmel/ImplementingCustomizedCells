a57bdfb6c7759672c9cea987c829ceceb31b9f87
Pedro Henrique <78853588+pedrohemmel@users.noreply.github.com>
2022-04-13 14:47:46 -0300

    First commit
    Criação do projeto no xcode

---
+++ ImplementingCustomizedCells/ImplementingCustomizedCells/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImplementingCustomizedCells
+//
+//  Created by Pedro Henrique Dias Hemmel de Oliveira Souza on 13/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
